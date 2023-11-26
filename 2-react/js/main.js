11a5c22007579b75fb58db9cc42437fe031aa84c
jueun jin <74699797+pearlag@users.noreply.github.com>
2023-11-26 23:53:47 +0900

    Add files via upload

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,8 +1,119 @@
-// TODO
-const element = (
-<header>
-  <h2 className="container">검색</h2>
-</header>
-);
+import store from "./js/Store.js";
 
-ReactDOM.render(element, document.querySelector("#app"));
+const TabType = {
+  KEYWORD: "KEYWORD",
+  HISTORY: "HISTORY",
+};
+
+const TabLabel = {
+  [TabType.KEYWORD]: "추천 검색어",
+  [TabType.HISTORY]: "최근 검색어",
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+      selectedTab: TabType.KEYWORD,
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.search(this.state.searchKeyword);
+  }
+
+  search(searchKeyword) {
+    const searchResult = store.search(searchKeyword);
+    this.setState({
+      searchResult,
+      submitted: true,
+    });
+  }
+
+  handleReset() {
+    this.setState({
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+    });
+  }
+
+  handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+
+    if (searchKeyword.length <= 0 && this.state.submitted) {
+      return this.handleReset();
+    }
+
+    this.setState({ searchKeyword });
+  }
+
+  render() {
+    const searchForm = (
+      <form
+        onSubmit={(event) => this.handleSubmit(event)}
+        onReset={() => this.handleReset()}
+      >
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          autoFocus
+          value={this.state.searchKeyword}
+          onChange={(event) => this.handleChangeInput(event)}
+        />
+        {this.state.searchKeyword.length > 0 && (
+          <button type="reset" className="btn-reset"></button>
+        )}
+      </form>
+    );
+
+    const searchResult =
+      this.state.searchResult.length > 0 ? (
+        <ul className="result">
+          {this.state.searchResult.map(({ id, imageUrl, name }) => (
+            <li key={id}>
+              <img src={imageUrl} />
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty-box">검색 결과가 없습니다</div>
+      );
+
+    const tabs = (
+      <>
+      <ul className="tabs">
+        {Object.values(TabType).map((tabType) => (
+          <li className={this.state.selectedTab === tabType ? 'active' : ''} key={tabType}
+          onClick={() => this.setState({selectedTab: tabType})}
+          >{TabLabel[tabType]}</li>
+        ))}
+      </ul>
+      {this.state.selectedTab === TabType.KEYWORD && <>TODO: 추천 검색어</>}
+      {this.state.selectedTab === TabType.HISTORY && <>TODO: 최근 검색어</>}
+      </>
+    );
+
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          {searchForm}
+          <div className="content">
+            {this.state.submitted ? searchResult : tabs}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
--- README.md
+++ README.md
@@ -25,3 +25,52 @@ const element = (
 ```
 > 카멜케이스만 사용.
 > class 대신 className
+
+
+## 템플릿 언어
+리액트에서는 JSX(JavaScript XML)을 사용한다.
+자바스크립트의 확장 문법.
+
+- 카멜케이스
+class -> className
+placeholder -> placeHolder
+autofocus -> autoFocus
+
+- 모두를 감싸는 하나의 부모노드가 있어야 함. (루트 노드가 여러개일 경우)
+```html
+<>
+  <header>
+    <h2 className="container">검색</h2>
+  </header>
+  <div className="container">
+    <form id="search-form-view">
+      ...
+      </form>
+  </div>
+</>
+```
+- 이벤트 핸들러
+onChange={}
+리액트에서 이벤트 처리하는 핸들러 이름은 접두사로 handle을 넣는다.
+ex) handleChangeInput
+
+
+- 조건부 렌더링
+1. 엘리먼트 변수를 사용하는 방식
+2. 삼항 연산자를 사용하는 방식
+{this.state.searchKeyword.length > 0 ? <button type="reset" className="btn-reset"></button> : null }
+
+3. && 연산자를 사용하는 방식
+{this.state.searchKeyword.length > 0 && (<button type="reset" className="btn-reset"></button>)}
+
+
+## setState
+비동기로 동작한다.
+컴포넌트 갱신 요청.
+
+## li key
+li에 key값 설정해야 함
+<li key={item.id}>
+보통은 id값을 넣는다.
+
+키값으로 index를 넣게 되면(두번째 파라미터 값으로도 넣어줘야 함.), 성능 저하나 화면이 갱신되지 않는 문제 내포.
